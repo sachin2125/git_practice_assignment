6f82874872c0c74c657b19ce33e3f24653f97317
unknown <maliksachin298@gmail.com>
2023-04-18 18:15:29 +0530

    initial commit

---
+++ prime.js
@@ -0,0 +1,11 @@
+
+  let count = 0;
+  for(i=1;i<=num;i++){
+    if(num%i==0){
+      count++;
+    }
+  }if(count==2){
+    console.log(num,"is a prime number");
+  }else{
+    console.log(num,"is  a prime number");
+  }
